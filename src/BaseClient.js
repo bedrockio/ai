a957797f7f88413b0c31c6c40eda1904b62d9904
Andrew Plummer <plummer.andrew@gmail.com>
2026-03-18 02:02:10 +0900

    Coerce "empty" message to a single period for Anthropic.

--- src/BaseClient.js
+++ src/BaseClient.js
@@ -137,7 +137,8 @@ export default class BaseClient {
   getFilteredMessages(options) {
     const { messages = [] } = options;
     return messages.filter((message) => {
-      return message.content.trim();
+      const trimmed = message.content.trim();
+      return trimmed && trimmed !== '.';
     });
   }
 
@@ -291,12 +292,14 @@ export default class BaseClient {
 
     if (result.length === 1 && !result[0].content) {
       // If a single user input is passed and is nullish, coerce it to a
-      // single space. Combined with getFilteredMessages below this allows
+      // single period. Combined with getFilteredMessages below this allows
       // a chatbot the ability to "speak first" by prompting it with empty
       // content. The empty message will be filtered out of the final result
-      // appearing as if the chatbot went first. Note empty string will also
-      // fail here hence the single space.
-      result[0].content = ' ';
+      // appearing as if the chatbot went first.
+      // Note that:
+      // GPT will fail on an empty string but on whitespace
+      // Anthropic will fail on all whitespace
+      result[0].content = '.';
     }
 
     return result;
